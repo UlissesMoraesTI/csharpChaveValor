0bb99db2e68860d53c74a88763877ce9ff69c03b
Ulisses Moraes <ulissesmoraes74@gmail.com>
2023-11-10 22:11:00 -0400

    Dictionary | Coleção Chave Valor

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
+
+foreach (var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
